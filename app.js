212db54241fbe6d656f2cd7661b857d2fe48dd06
Alexander Kalinin <alec.kalinin@gmail.com>
2019-01-29 10:11:12 +0100

    Clean async/await code

--- app.js
+++ app.js
@@ -2,65 +2,25 @@ var firebase = require("firebase");
 var config = require("./config");
 
 firebase.initializeApp(config);
+var db = firebase.firestore();
 
-login()
-.then(db_save);
-
-async function login() {
-  console.log('A');
-  firebase.auth().signInAnonymously()
-  .catch(error => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
-}
-
-async function db_save() {
-  print('B');
-  var db = firebase.firestore();
-  
-  db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  })
-  .then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
+(async function() {
+  try {
+    auth = await firebase.auth().signInAnonymously()
+    console.log("User ID: "+ auth.user.uid);
+    
+    document = await db.collection("users").add({
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    });
+    console.log("Document ID: "+ document.id);
+    
     process.exit(0);
-  })
-  .catch(function(error) {
-    console.error("Error adding document: ", error);
-  });
-}
+  }
+  catch (error) {
+    console.log(error);
+  }
+})();
 
-// firebase.auth().signInAnonymously()
-// .then(response => {
-//   (async function() {
-//     await database();
-//     console.log('B');
-//   })();
-// })
-// .catch(error => {
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-// });
-
-// console.log('A');
-
-// async function database() {
-//   var db = firebase.firestore();
-//   console.log(firebase.auth().currentUser.uid);
-  
-//   db.collection("users").add({
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815
-//   })
-//   .then(function(docRef) {
-//     console.log("Document written with ID: ", docRef.id);
-//     process.exit(0);
-//   })
-//   .catch(function(error) {
-//     console.error("Error adding document: ", error);
-//   });
-// }
+console.log('A');
