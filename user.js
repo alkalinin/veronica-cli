0a6914d7fd735788e15baf0d18fc8813ec7fefed
Alexander Kalinin <alec.kalinin@gmail.com>
2019-01-29 14:36:25 +0100

    Implement prototype for the admin data read

--- user.js
+++ user.js
@@ -4,23 +4,39 @@ var config = require("./config");
 firebase.initializeApp(config);
 var db = firebase.firestore();
 
-(async function() {
+var write = async () => {
   try {
     auth = await firebase.auth().signInAnonymously()
     console.log("User ID: "+ auth.user.uid);
     
-    document = await db.collection("users").add({
+    docRef = await db.collection("users").add({
       first: "Ada",
       last: "Lovelace",
       born: 1815
     });
-    console.log("Document ID: "+ document.id);
-    
+    console.log("Document ID: "+ docRef.id);
+
     process.exit(0);
   }
   catch (error) {
     console.log(error);
   }
-})();
+};
+
+var read = async () => {
+  var usersRef = db.collection("users");
+  usersRef.get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+      })
+    })
+    .catch(err => {
+      console.log(error);
+    })
+}
+
+write();
+// read();
 
 console.log('A');
