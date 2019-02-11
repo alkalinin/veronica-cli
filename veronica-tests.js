04ef96ef839e507a0da4f153c39c929753826dff
Alexander Kalinin <alec.kalinin@gmail.com>
2019-02-11 17:38:17 +0100

    Update veronica tests

--- veronica-tests.js
+++ veronica-tests.js
@@ -1,6 +1,6 @@
-const firebase = require("firebase");
+const firebase = require('firebase');
 
-const config = require("./config.json");
+const config = require('./config.json');
 
 /**
  * Firebase: initialize user API
@@ -10,34 +10,40 @@ firebase.initializeApp(config);
 const db = firebase.firestore();
 
 var createTestDocument = async () => {
+  console.log('TEST: Create Document');
   try {
     auth = await firebase.auth().signInAnonymously()
-    console.log("User ID: "+ auth.user.uid);
+    console.log('User Id: ' + auth.user.uid);
     
-    docRef = await db.collection("users").add({
+    docRef = await db.collection('users').add({
       answers: [0, 1, 2, 3, 4, 5]
     });
-    console.log("Document ID: "+ docRef.id);
-
-    process.exit(0);
+    console.log('Document Id: ' + docRef.id);
   }
   catch (error) {
     console.log(error);
   }
+  console.log('');
 };
 
 var readTestDocument = async () => {
-  var usersRef = db.collection("users");
-  usersRef.get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-        console.log(doc.id, '=>', doc.data());
-      });
-    })
-    .catch(err => {
-      console.log(error);
+  console.log('TEST: Read Document');
+
+  console.log('User Id: ' + auth.user.uid);
+  try {
+    var usersRef = db.collection('users');
+    snapshot = await usersRef.get();
+    snapshot.forEach(doc => {
+      console.log('Document Id: ' + doc.id);
     });
+  } catch(error) {
+    console.log(error);
+  }
+  console.log('');
 }
 
-createTestDocument();
-readTestDocument();
+(async () => {
+  await createTestDocument();
+  await readTestDocument();
+  process.exit(0);
+})();
