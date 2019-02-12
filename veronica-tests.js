2685b1fcf80ce8c1920deffa2f4bd0a93436bffa
Alexander Kalinin <alec.kalinin@gmail.com>
2019-02-12 11:48:46 +0100

    Update test set for firestore

--- veronica-tests.js
+++ veronica-tests.js
@@ -7,43 +7,128 @@ const config = require('./config.json');
  */
 firebase.initializeApp(config);
 
-const db = firebase.firestore();
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
-var createTestDocument = async () => {
-  console.log('TEST: Create Document');
+/**
+ * Create Documents Test Set
+ */
+var createDocumentsTest01 = async () => {
+  console.log('  01 Non authenticated user cannot create document');
+
+  var passed = false;
+
   try {
-    auth = await firebase.auth().signInAnonymously()
-    console.log('User Id: ' + auth.user.uid);
-    
-    docRef = await db.collection('users').doc(auth.user.uid).set({
+    doc = await firestore.collection('users').add({
       answers: [0, 1, 2, 3, 4, 5]
     });
-    console.log('Document Id: ' + docRef.id);
+  } catch (error) {
+    if (error.code === 'permission-denied') {
+      passed = true;
+    }
   }
-  catch (error) {
-    console.log(error);
-  }
-  console.log('');
-};
 
-var readTestDocument = async () => {
-  console.log('TEST: Read Document');
-
-  console.log('User Id: ' + auth.user.uid);
-  try {
-    var collectionRef = db.collection('users');
-    snapshot = await collectionRef.get();
-    snapshot.forEach(doc => {
-      console.log('Document Id: ' + doc.id);
-    });
-  } catch(error) {
-    console.log(error);
+  if (passed) {
+    console.log('  OK');
+  } else {
+    console.log('  FAILED');
   }
+
   console.log('');
 }
 
+var createDocumentsTest02 = async () => {
+  console.log('  02 Authenticated user can create document');
+
+  var passed = false;
+
+  try {
+    userRecord = await auth.signInAnonymously();
+
+    await firestore.collection('users').doc(userRecord.user.uid).set({
+      answers: [0, 1, 2, 3, 4, 5]
+    });
+
+    await auth.signOut();
+
+    passed = true;
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (passed) {
+    console.log('  OK');
+  } else {
+    console.log('  FAILED');
+  }
+
+  console.log('');
+}
+
+/**
+ * Read Documents Test Set
+ */
+var readDocumentsTest01 = async () => {
+  console.log('  01 Non authenticated user cannot read list of all document');
+
+  var passed = false;
+
+  try {
+    var collectionRef = firestore.collection('users');
+    var snapshot = await collectionRef.get();
+  }
+  catch (error) {
+    if (error.code === 'permission-denied') {
+      passed = true;
+    }
+  }
+
+  if (passed) {
+    console.log('  OK');
+  } else {
+    console.log('  FAILED');
+  }
+
+  console.log('');
+}
+
+var readDocumentsTest02 = async () => {
+  console.log('  02 Authenticated user cannot read list of all document');
+
+  var passed = false;
+
+  try {
+    userRecord = await auth.signInAnonymously();
+
+    var collectionRef = firestore.collection('users');
+    var snapshot = await collectionRef.get();
+  }
+  catch (error) {
+    if (error.code === 'permission-denied') {
+      passed = true;
+    }
+  }
+
+  if (passed) {
+    console.log('  OK');
+  } else {
+    console.log('  FAILED');
+  }
+
+  console.log('');
+}
+
+/**
+ * Execute all tests set
+ */
 (async () => {
-  await createTestDocument();
-  await readTestDocument();
+  console.log('TEST: Create Documents');
+  await createDocumentsTest01();
+  await createDocumentsTest02();
+
+  console.log('TEST: Read Documents');
+  await readDocumentsTest01();
+  await readDocumentsTest02();
+
   process.exit(0);
 })();
